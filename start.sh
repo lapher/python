2e91fccba941acd5c7af1c39f2caa958e98a5318
lapher <cclapher@gmail.com>
2017-05-21 18:19:09 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,8 @@
+sudo yum install docker
+sudo service docker start
+sudo chkconfig docker on
+sudo docker build -t python_bug docker/
+cd bug
+sudo wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
+sudo tar xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
+sudo docker run -it --rm -v /home/ec2-user/python/bug/:/home python_bug
